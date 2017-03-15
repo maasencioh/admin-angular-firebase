cac8245da2788b48ca7f7feeab3d9242ee228b27
maasencioh <maasencioh@gmail.com>
2017-03-14 22:10:22 -0500

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # admin-angular-firebase
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
+
+## License
+
+  - [MIT](./LICENSE)
---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adminAngularFirebaseApp
+ * @description
+ * # adminAngularFirebaseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adminAngularFirebaseApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
